03c17ec1f8f5e8d5b3317b3e4f22300ceccdab2e
gabscostah <oliveiramariagabriela020@gmail.com>
2024-05-16 10:27:57 -0300

    Criar função calcular

---
+++ SRC/Components/form/ResultImc/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function ResultImc(props){
+    return(
+        <view>
+            <text>{props.messageResultImc}</text>
+            <text>{props.ResultImc}</text>
+        </view>
+    );
+    }
+    
